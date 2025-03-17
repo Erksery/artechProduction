06083987a56306e85b04dc9920de0973bdb3c5e5
Erksery <114258961+Erksery@users.noreply.github.com>
2025-03-17 23:38:58 +0300

    correct

---
+++ src/components/layouts/folder/folderList/FolderList.tsx
@@ -0,0 +1,42 @@
+import React, { useMemo, useState, useEffect } from "react";
+import { Reorder, motion } from "framer-motion";
+import styles from "./FolderList.module.scss";
+import { FolderCard } from "../folderCard/FolderCard";
+import { FolderData } from "../../../../interfaces/folder";
+
+interface FolderListProps {
+  folders: FolderData[];
+}
+
+export const FolderList: React.FC<FolderListProps> = ({ folders }) => {
+  const parentFolders = useMemo(() => {
+    return folders.filter((folder) => folder.inFolder === null);
+  }, [folders]);
+
+  const [orderedFolders, setOrderedFolders] = useState<FolderData[]>(() => {
+    const storedFolders = localStorage.getItem("orderedFolders");
+    return storedFolders ? JSON.parse(storedFolders) : [];
+  });
+
+  useEffect(() => {
+    if (folders.length > 0 && orderedFolders.length === 0) {
+      setOrderedFolders(parentFolders);
+    }
+  }, [folders]);
+
+  return (
+    <Reorder.Group
+      axis="y"
+      values={orderedFolders}
+      onReorder={setOrderedFolders}
+      className={styles.list}
+      as="div"
+    >
+      {orderedFolders.map((folder) => (
+        <Reorder.Item key={folder.id} value={folder} as="div">
+          <FolderCard folder={folder} folders={folders} />
+        </Reorder.Item>
+      ))}
+    </Reorder.Group>
+  );
+};
--- src/pages/Registration/hooks/useRegUser.ts
+++ src/pages/Registration/hooks/useRegUser.ts
@@ -6,7 +6,6 @@ import { AppDispatch } from "../../../store";
 import { setUserData } from "../../../store/slices/user";
 import { User } from "../../../interfaces/user";
 import { useNavigate } from "react-router-dom";
-import { API_ROUTES } from "../../../api/routes";
 
 interface RegistrationData {
   login: string;
@@ -31,7 +30,7 @@ export const useRegUser = () => {
 
     try {
       const resData: AxiosResponse<RegistrationResponse> = await api.post(
-        API_ROUTES.REGISTER,
+        "/auth/register",
         {
           login: value.login,
           password: value.password,
